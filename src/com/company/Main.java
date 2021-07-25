8e2b9110e34f285fae2b9bb3f3ed422cae97bbf1
AVUniverse <rgbavuniverse@gmail.com>
2021-07-25 18:10:49 +0530

    2nd Mothed is added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,12 @@ public class Main {
 	// Method-1 to add two numbers
 
         System.out.println(4+3);
+
+    // Method-2 to add two numbers
+
+        int a=5;
+        int b=4;
+        System.out.println(a+b);
+        
     }
 }
