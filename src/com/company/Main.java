db2c5503ab736fe4b523ee26742a98d8d93ab212
AVUniverse <rgbavuniverse@gmail.com>
2021-07-25 18:37:09 +0530

    4th Method is added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// Method-1 to add two numbers
+	// Method-1 to add two numbers 4+3
 
         System.out.println(4+3);
 
-    // Method-2 to add two numbers
+    // Method-2 to add two numbers 4+5
 
         int a=5;
         int b=4;
         System.out.println(a+b);
 
-    // Method-3 to add two numbers
+    // Method-3 to add two numbers 10+5
 
         int c=10, d=5;
         int sum = c+d;
